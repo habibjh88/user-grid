753f56a127acd7dfa5857e9a01bd7c5fdf835926
Habib Rahaman <habibjh88@gmail.com>
2024-06-23 17:49:30 +0600

    style changed

--- assets/blocks/main.asset.php
+++ assets/blocks/main.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '484167352d232304ef2d');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'dd50d961d15777096d1b');
